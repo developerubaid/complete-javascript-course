1bb2dc6f5d4501590fc65d7febd5fefc26035f6b
Hafiz Ubaidullah Anwar <developerubaid@gmail.com>
2025-11-20 15:35:31 +0500

    feat: add implementation for modal handling

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// console.log(btnsOpenModal);
+
+const handleModalOpen = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+
+const handleModalClose = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnsOpenModal.forEach(btn => {
+    btn.addEventListener(
+        'click', handleModalOpen
+    );
+});
+
+btnCloseModal.addEventListener(
+    'click', handleModalClose
+);
+
+overlay.addEventListener(
+    'click', handleModalClose
+);
+
+document.addEventListener('keydown', function(event) {
+    // console.log(event.key);
+    if(event.key === 'Escape' && !modal.classList.contains('hidden'))
+        handleModalClose();
+});
